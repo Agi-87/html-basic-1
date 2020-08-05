4151506eec102af92ac6e7e8cd80a4c91743ee92
Agi-87 <m.agnes865@gmail.com>
2020-08-05 15:09:37 +0200

    firssítés

--- js/main.js
+++ js/main.js
@@ -2,8 +2,25 @@ function calcAmount () {
     let price = 1000;
     let amountInput = document.querySelector("input[name='amount-input']");
     let showAmount = document.querySelector("span.show-amount");
-    let amount = parseInt(amountInput.value) * price;
-    
+    let amountNumber = parseInt (amountInput.value);
+
+amountNumber =    isNaN(amountNumber) ? 0: amountNumber;  
+
+if(amountNumber > 10) {
+    alert("Maximum 10 terméket vásárolhat!");
+    return;
+} else if (amountNumber < 1) {
+   
+    alert("Minimum egy terméket kell vásárolnia!");
+} else {
+   
+    let amount = amountNumber * price;
     showAmount.innerHTML = amount;
+    
+}
+
+
+
+
     
 }
