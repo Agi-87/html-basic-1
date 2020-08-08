c6bfeea3abbf909b8fe172c71ef07e62a8ac01c4
Agi-87 <m.agnes865@gmail.com>
2020-08-08 18:02:17 +0200

    Frissítések

---
+++ js/objectum.js
@@ -0,0 +1,30 @@
+/*
+let user = {
+    name: "Kiss Ramóna",
+    age: 20,
+    city: "Bp"
+};
+
+let admin = new Object();
+
+let customer = {};
+
+user.name
+
+Object.keys(user);
+(3)["name", "age", "city"]
+Object.values(user);
+(3)["Kiss Ramóna", 20, "Bp"]
+Object.entries(user);
+(3)[Array(2), Array(2), Array(2)]0: (2)["name", "Kiss Ramóna"]1: (2)["age", 20]2: (2)["city", "Bp"]length: 3__proto__: Array(0)
+
+Object.entries(user) [1] [0]
+"age"
+
+Object.entries(user).length
+3
+Object.values(user).length
+3
+Object.entries(user).length
+3
+*/
