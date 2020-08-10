ef96fee80ff104aa3457a1558e00678dc25f9241
Agi-87 <m.agnes865@gmail.com>
2020-08-10 11:47:52 +0200

    Refresh sources

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ function calcAmount() {
     let amountNumber = parseInt(amountInput.value);
     amountNumber = isNaN(amountNumber) ? 0 : amountNumber;
 
-    showSumPriece(price,amountNumber);
+    showSumPriece(price,amountNumber);}
 
     function showSumPriece(price =1000, amountNumber= 1) {
         amountNumber = amountNumber || 0;
@@ -19,6 +19,71 @@ function calcAmount() {
         }
     }
 
+// Add helptext.
+let helpText = document.createElement("small");
+helpText.className = "form-text text-muted";
+helpText.innerHTML = "Adja meg a feltéteket!";
 
+let parent = document.querySelector("div.form-group:nth-child(1)");
+parent.appendChild(helpText);
 
+parent.removeChild(helpText);
+
+//Window events.
+let sendButton = document.querySelector("form .btn.btn-primary");
+/*
+sendButton.onclick = function() {
+    alert("Hello JS!");
+}
+*/
+
+sendButton.addEventListener("click", function() {
+    //alert("Hello JS!");
+});
+
+window.addEventListener("resize", function() {
+    console.log(this.innerHeight, this.innerWidth);
+});
+
+//Űrlap események:
+let orderForm = document.querySelector("#orderForm");
+orderForm.addEventListener("submit", function(ev){
+ev.preventDefault();
+
+let inputs = this.querySelectorAll("input");
+console.log(inputs);
+let values = {};
+for (let i =0; i <inputs.length; i++) {
+    values[inputs[i].name] = inputs[i].value;
+}
+
+console.log( values );
+
+});
+
+//Parent element kezelése.
+let alertCloseButtons = document.querySelectorAll(".close[data-dismiss='alert']");
+let alertCloseEventHandlerFunction = function(ev) {
+    this.parentElement.style.display = "none";
+};
+for(let i = 0; i < alertCloseButtons.length; i++) {
+    alertCloseButtons[i].addEventListener("click", alertCloseEventHandlerFunction );
+}
+
+//Select elem kitöltése.
+let toppings = [
+    "Szalonna",
+    "Hagyma",
+    "Tükörtojás",
+    "Libamáj",
+    "Extra Sonka"
+];
+let toppingSelect = document.querySelector("#topInput");
+let index = 0;
+while(index < toppings.length) {
+    let option = document.createElement("option");
+    option.value = index;
+    option.innerHTML = toppings[index];
+    toppingSelect.appendChild(option);
+    index++;
 }
